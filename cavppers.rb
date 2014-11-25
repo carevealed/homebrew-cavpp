b0077471b965dfd6778a73eaebfd65fc24a864aa
Dave Rice <dave@dericed.com>
2014-11-24 23:06:07 -0500

    update path to cavpp repo

--- cavppers.rb
+++ cavppers.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Cavppers < Formula
-  homepage 'https://github.com/dericed/cavppers'
-  url 'https://github.com/dericed/cavppers/archive/cavppers_v0.5.1.zip'
+  homepage 'https://github.com/cavpp/cavppers'
+  url 'https://github.com/cavpp/cavppers/archive/cavppers_v0.5.1.zip'
   sha256 'd48b6cbfd05883fff29603d41fe08e66030460cab5dada4791547563fb5be35b'
   
-  head 'https://github.com/dericed/cavppers.git'
+  head 'https://github.com/cavpp/cavppers.git'
 
   depends_on 'ffmpeg'
   depends_on 'mediainfo'
