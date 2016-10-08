ca57822dd3626cb5c052f3871f76e178a57661cb
Dave Rice <dave@dericed.com>
2016-10-08 17:23:49 -0400

    simplify ffmpeg config

--- cavppers.rb
+++ cavppers.rb
@@ -4,11 +4,11 @@ class Cavppers < Formula
   homepage 'https://github.com/cavpp/cavppers'
   url 'https://github.com/cavpp/cavppers/archive/cavppers_v0.5.3.zip'
   sha256 'c08d54c116778a407285901c7fecdcadb4f9cfeab6064fa6899f722ac3b33b44'
-  revision 1
+  revision 2
   
   head 'https://github.com/cavpp/cavppers.git'
 
-  depends_on 'ffmpeg' => [ 'with-ffplay', 'with-openjpeg', 'with-libass', 'with-freetype' , 'with-faac' ]
+  depends_on 'ffmpeg' => [ 'with-openjpeg', 'with-libass', 'with-freetype' ]
   depends_on 'mediainfo'
 
   def install
