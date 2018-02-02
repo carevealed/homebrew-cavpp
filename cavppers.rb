f3c1b1baf9f728d9036344b57fdd7898d5016dbc
Dave Rice <dave@dericed.com>
2018-02-02 12:57:12 -0500

    cavppers_v0.5.4

--- cavppers.rb
+++ cavppers.rb
@@ -2,16 +2,19 @@ require 'formula'
 
 class Cavppers < Formula
   homepage 'https://github.com/cavpp/cavppers'
-  url 'https://github.com/cavpp/cavppers/archive/cavppers_v0.5.3.zip'
-  sha256 'c08d54c116778a407285901c7fecdcadb4f9cfeab6064fa6899f722ac3b33b44'
-  revision 2
+  url 'https://github.com/cavpp/cavppers/archive/cavppers_v0.5.4.zip'
+  sha256 'b51f7320520a3792e835bba30fdb0dd93016c11a626e6d53a0e0e84e920b1e0e'
   
   head 'https://github.com/cavpp/cavppers.git'
 
   depends_on 'ffmpeg' => [ 'with-openjpeg', 'with-libass', 'with-freetype' ]
   depends_on 'mediainfo'
+  depends_on 'csvprintf'
+  depends_on 'xmlstarlet'
 
   def install
+     bin.install 'csv2pbcore.xsl'
+     bin.install 'pbcorethat'
      bin.install 'makeprestarget'
      bin.install 'makeaccesstarget'
      bin.install 'cavpperfunctions'
